e8c78b80bacb6ea2f4f6c7a09d17d8bccacd4f20
David Gleba <dgleba@gmail.com>
2019-03-08 23:06:23 -0500

    2019-03-08c

---
+++ vpv445/unused/main (3.8.2019 move to 445g).js
@@ -0,0 +1,21 @@
+
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
+
+//  
+//  dg-version-vpv445_v02
+//  2019-03-04_Mon_08.19-AM
+// 
+
+
+
+// let id = null;
+// let _id = null;
+
+const routes = [
+  
+];
+
+const router = new VueRouter({ mode: "history", routes: routes });
+
+new Vue(Vue.util.extend({ router }, App)).$mount("#app");
