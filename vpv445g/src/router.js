dd3cc408376936e65af72eac6db2e6ae02ee78e1
David Gleba <dgleba@gmail.com>
2019-03-30 16:23:12 -0400

    moved settings to app.vue

--- vpv445g/src/router.js
+++ vpv445g/src/router.js
@@ -1,10 +1,9 @@
 import Vue from "vue";
 import Router from "vue-router";
 
-
 // import Home from "./views/Home.vue";
 
-import appsettings from "./components/appsettings.vue";
+// import appsettings from "./components/appsettings.vue";
 
 import IndexComponent from "./components/IndexComponent.vue";
 import CreateComponent from "./components/CreateComponent.vue";
@@ -16,23 +15,26 @@ import HomeComponent from "./components/HomeComponent.vue";
 
 Vue.use(Router);
 
+
+// {
+//   path: "/settings",
+//   component: appsettings
+// },
+
+
 export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
-      path: "/settings",
-      component: appsettings
+      name: "posts",
+      path: "/",
+      component: IndexComponent
     },
     {
       name: "create",
       path: "/create",
       component: CreateComponent
-    },
-    {
-      name: "posts",
-      path: "/",
-      component: IndexComponent
     },
     {
       name: "edit",
